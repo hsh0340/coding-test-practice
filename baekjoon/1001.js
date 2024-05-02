9d649c544b3944ef4c35f6d97eb939498f49ac1f
hsh0340 <hsh0340@naver.com>
2024-05-02 18:17:03 +0900

    테스트 용 문제  삭제

--- baekjoon/1001.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-// const input = fs.readFileSync("input/1001.txt").toString().trim().split(" ");
-const input = fs.readFileSync('/dev/stdin').toString().trim().split(' ');
-const a = +input[0];
-const b = +input[1];
-const result = a - b;
-console.log(result);
